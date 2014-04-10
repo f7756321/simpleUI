6ca7d4444985c9e042900288832b45a20fda0868
Alan <jyunlong@ourpower.com.tw>
2014-04-10 22:04:46 +0800

    UI

---
+++ src/com/example/simpleui/MainActivity.java
@@ -0,0 +1,142 @@
+package com.example.simpleui;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnKeyListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.os.Build;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    
+    
+    
+//    public void send(View view)
+//    {
+//    	Log.d("debug","123");
+
+//    }
+    
+    
+    
+    
+    
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+    	
+    	
+    	private Button button;
+    	private EditText editText;
+    	private CheckBox checkBox;
+    	
+    	
+    	private void send()
+    	{
+    		
+    		String text = editText.getText().toString();
+    		if(checkBox.isChecked())
+    		{
+    			text="***********";
+    		}
+			Toast.makeText(getActivity(), text, Toast.LENGTH_SHORT)
+					.show();
+			editText.setText("");
+    	}
+    	
+    	
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            
+            checkBox= (CheckBox) rootView.findViewById(R.id.checkBox1);
+            button= (Button) rootView.findViewById(R.id.button1);
+            editText = (EditText) rootView.findViewById(R.id.editText1);
+            
+            button.setText("µo°e");
+            
+            editText.setOnKeyListener(new OnKeyListener() {
+				
+				@Override
+				public boolean onKey(View v, int keyCode, KeyEvent event) {
+					// TODO Auto-generated method stub
+					if(event.getAction()==KeyEvent.ACTION_DOWN && keyCode==KeyEvent.KEYCODE_ENTER)
+					{
+						send();
+						return true;
+					}
+					
+					
+					return false;
+				}
+			});
+            
+            
+            button.setOnClickListener(new OnClickListener(){
+
+				@Override
+				public void onClick(View arg0) {
+					// TODO Auto-generated method stub
+					
+					send();
+					
+				}});
+            
+            
+            return rootView;
+        }
+    }
+
+}
